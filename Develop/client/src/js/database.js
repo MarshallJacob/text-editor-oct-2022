56a1711b791e8e7dfc386365d41f43060c9762bb
Mjacke <marshall.jacob16@icloud.com>
2022-11-02 13:03:33 -0600

    completed most of the TODOs. Testing for functionality and then finalizing application.

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,38 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // use multiple methods to connect to the database and add content to that database
+  const jateDB = await openDB('jate', 1);
+  const text = jateDB.transaction('jate', 'readwrite');
+  const store = text.objectStore('jate');
+  const update = store.put({id: 1, value: content});
+  const result = await update;
+  console.log('result', result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  // similar process using different methods to comunicate with the data base. This time it will return data similar to a GET route method.
+  const jateDB = await openDB('jate', 1);
+  const text = jateDB.transaction('jate', 'readwrite');
+  const store = text.objectStore('jate');
+  const pull = store.getAll();
+  const data = await pull;
+  console.log(data);
+  return data?.[0]?.value;
+};
+
+initdb();
